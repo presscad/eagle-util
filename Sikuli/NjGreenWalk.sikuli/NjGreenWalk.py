16d1b46aa1382077888456c876be8353a35a6f43
Ying Dai <ying.dai@sap.com>
2016-10-06 19:12:56 +0800

    Sikuli/NjGreenWalk.sikuli: updated

--- Sikuli/NjGreenWalk.sikuli/NjGreenWalk.py
+++ Sikuli/NjGreenWalk.sikuli/NjGreenWalk.py
@@ -1,5 +1,33 @@
 USER = '1385***4288'
 PASSWORD = '******'
+IMEI = ''
+
+# if IMEI is an empty string, create one
+def setNoxMEID(phoneNum, IMEI):
+    clearToMain()
+    find("nox-simulator.png")
+    click("nox-setup.png")
+    wait("systemConfig.png")
+    click("property-setup.png")
+
+    r = find("phone-num.png").right(100)
+    doubleClick(r)
+    type(Key.BACKSPACE)
+    paste(phoneNum)
+
+    if "" != IMEI:
+        r = find("ImeiConfig.png").right(100)
+        doubleClick(r)
+        type(Key.BACKSPACE)
+        paste(IMEI)
+    else:
+        click("sys-setting-imei-create.png")
+
+    click("save-config.png")
+    if exists("sysUpdateSuccess.png"):
+        click("OK.png")
+    r = find("systemConfig.png").right(600)
+    click(r.find("close.png"))
 
 def logOn(user, password):
     click("mynanjing-app-icon.png")
@@ -16,13 +44,15 @@ def clearToMain():
 
 def main():
     import datetime
-    
+
+    setNoxMEID(USER, IMEI)
     clearToMain()
     logOn(USER, PASSWORD)
+    clearToMain()
+
     for k in range(20000):
-        #st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        #print(st + ': k=' + str(k))
+        #print(str(datetime.datetime.now()) + ': k=' + str(k))
         click("nox-right-bar-shake.png")
-        time.sleep(0.8)
+        time.sleep(0.5)
 
 main()
