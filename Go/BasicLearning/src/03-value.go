35a4a9b738bec470f1d59641827b759ffaaec4df
Eagle Dai <dynuaa@gmail.com>
2019-07-12 17:49:27 +0800

    Go/BasicLearning/src/02-09

---
+++ Go/BasicLearning/src/03-value.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // 字符串可以通过 `+` 连接。
+    fmt.Println("go" + "lang")
+
+    // 整数和浮点数
+    fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println("7/3 =", 7/3)
+	fmt.Println("7.0/3 =", 7.0/3)
+
+    // 布尔型，还有你想要的逻辑运算符。
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
