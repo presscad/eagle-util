c7233ca9b092f36ba1925a89d6a8a4ce3567f884
Eagle Dai <dynuaa@gmail.com>
2019-08-12 10:22:51 +0800

    Python/monitor-wifi.py fix

--- Python/monitor-wifi.py
+++ Python/monitor-wifi.py
@@ -1,5 +1,11 @@
 #https://stackoverflow.com/questions/14077835/associating-my-windows-computer-to-a-wifi-ap-with-python
+
 import subprocess
+import time
+import traceback
+import sys
+import datetime
+
 
 ADAPERS = [
     ['48-8A-D2-75-12-60', 'aWiFi'],
@@ -7,12 +13,18 @@ ADAPERS = [
     ['48-8A-D2-E0-9C-21', 'SAP-Internet']
 ]
 
+def decode_bytes_to_str(bys):
+    str = ''
+    for b in bys:
+        str += chr(b)
+    return str
+
 
 def disconnect_wlan():
     process = subprocess.Popen('netsh wlan disconnect', shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    stdout = stdout.decode("utf-8")
+    stdout = decode_bytes_to_str(stdout)
 
     # Return `True` if we were able to successfully connect
     return ('successfully' in stdout) or ('\xd2\xd1\xb3\xc9\xb9\xa6' in stdout) #已成功...
@@ -21,13 +33,9 @@ def disconnect_wlan():
 def connect_to_network(interface, ssid):
     cmd = 'netsh wlan connect name={s} ssid={s} interface="{i}"'.format(i=interface, s=ssid)
     print(cmd)
-    process = subprocess.Popen(
-        cmd,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    stdout = stdout.decode("utf-8")
+    stdout = decode_bytes_to_str(stdout)
     print(stdout, stderr)
 
     # Return `True` if we were able to successfully connect
@@ -35,13 +43,9 @@ def connect_to_network(interface, ssid):
 
 
 def connected_ssids():
-    process = subprocess.Popen(
-        'netsh wlan show interfaces',
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
+    process = subprocess.Popen('netsh wlan show interfaces', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    stdout = stdout.decode("utf-8")
+    stdout = decode_bytes_to_str(stdout)
     #print(stdout, stderr)
 
     connected = []
@@ -73,7 +77,7 @@ def available_ssid():
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    stdout = stdout.decode("utf-8")
+    stdout = decode_bytes_to_str(stdout)
     #print(stdout, stderr)
 
     avai_ssids = []
@@ -107,14 +111,8 @@ def check_connected_ssids_valid(connected_ssids):
 def get_mac_to_wifi_adapter_dict():
     process = subprocess.Popen('ipconfig /all', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    stdout = stdout.replace((0xc4).to_bytes(1, 'little'), ' '.encode('utf-8'))
-    stdout = stdout.replace((0xea).to_bytes(1, 'little'), ' '.encode('utf-8'))
-    stdout = stdout.replace((0xd4).to_bytes(1, 'little'), ' '.encode('utf-8'))
-    stdout = stdout.replace((0xc2).to_bytes(1, 'little'), ' '.encode('utf-8'))
-    stdout = stdout.replace((0xc8).to_bytes(1, 'little'), ' '.encode('utf-8'))
-    stdout = stdout.replace((0xd5).to_bytes(1, 'little'), ' '.encode('utf-8'))
-    stdout = stdout.decode("utf-8")
-    #print(stdout, stderr)
+    stdout = decode_bytes_to_str(stdout)
+    #print(stdout)
 
     result = {}
     adapter = ''
@@ -134,22 +132,37 @@ def get_mac_to_wifi_adapter_dict():
     return result
 
 
-#from __future__ import print_function
-ssids = connected_ssids()
-print('Connected SSIDs:')
-for (adapter, ssid, mac) in ssids:
-    print('    %s (%s): %s' %(adapter, mac, ssid))
+def main():
+    #from __future__ import print_function
+    ssids = connected_ssids()
+    print('Connected SSIDs:')
+    for (adapter, ssid, mac) in ssids:
+        print('  %s (%s): %s' %(adapter, mac, ssid))
 
-if False == check_connected_ssids_valid(ssids):
-    mac2adapter = get_mac_to_wifi_adapter_dict()
+    if False == check_connected_ssids_valid(ssids):
+        mac2adapter = get_mac_to_wifi_adapter_dict()
 
-    for adapter in ADAPERS:
-        if adapter[2] == False:
-            mac, ssid = adapter[0], adapter[1]
-            print('Device "%s" is not connnected to "%s". Start connecting ...' %(mac, ssid))
-            try:
-                adapter = mac2adapter[mac]
-            except KeyError:
-                print('Device "%s" is not found!' %(mac))
-                continue
-            connect_to_network(adapter, ssid)
+        for adapter in ADAPERS:
+            if adapter[2] == False:
+                mac, ssid = adapter[0], adapter[1]
+                print('Device "%s" is not connnected to "%s". Start connecting ...' %(mac, ssid))
+                try:
+                    adapter = mac2adapter[mac]
+                except KeyError:
+                    print('Device "%s" is not found!' %(mac))
+                    continue
+                connect_to_network(adapter, ssid)
+
+
+while True:
+    st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    print('[%s] ' % st, end='')
+    try:
+        main()
+    except Exception as e:
+        print("Unexpected error:", traceback.format_exc())
+
+    s = 40.0
+    print('Sleep %s seconds ...' %(s))
+    sys.stdout.flush()
+    time.sleep(s)
